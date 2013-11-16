34e47f9736c7ba6c0b401c0c318b7c0f973f4c4f
quadodo <douglas.rennehan@gmail.com>
2013-11-16 15:22:04 -0800

    revamp
    Changing to location of stable release, see qls for development.

--- login.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('/includes/header.php');
-
-if (!$Entity->validEntity) {
-    require_once('/html/login_html.php');
-}
-else {
-    echo ALREADY_LOGGED_IN;
-}
--- register.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('/includes/header.php');
-
-if (!$Entity->validEntity) {
-    require_once('/html/register_html.php');
-}
-else {
-    echo ALREADY_LOGGED_IN;
-}
